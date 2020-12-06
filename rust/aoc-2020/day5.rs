033b4015b5b4e1ea4fe0c6fbd971b8c0db707b44
Joshua Skrzypek <jskrzypek@gmail.com>
2020-12-06 18:39:10 -0500

    feat: Day 5
    a day late still

--- rust/aoc-2020/day5.rs
+++ rust/aoc-2020/day5.rs
@@ -1,105 +1,108 @@
-use aoc_runner_derive::aoc;
-use std::cell::Cell;
-use std::num::NonZeroU8;
+use aoc_runner_derive::{aoc, aoc_generator};
+use std::num::ParseIntError;
+use std::cmp::Ordering;
+use std::ops::Range;
 
-const DIFF: u8 = b'a' - b'A';
+#[derive(Debug)]
+struct Seat {
+    row: u16,
+    col: u16,
+    sid: u16,
+    cod: String,
+}
 
-#[inline]
-fn diff(a: u8, b: u8) -> u8 {
-    u8::max(a, b) - u8::min(a, b)
+#[aoc_generator(day5)]
+// attempt 1... doesn't need to be this hard...
+// fn parse_input_day1(input: &str) -> Result<Vec<Seat>, &'static str> {
+//     input.lines().map(|l| -> Result<Seat, &'static str> {
+//         let (r, c) = l.split_at(7);
+//         println!("{:?} {:?}",r,c);
+//         let br = r.replace('F', "0").replace('B', "1");
+//         if let Ok(row) = u16::from_str_radix(&br, 2) {
+//             println!("{:?} {:?}",r,br);
+//             let bc = c.replace('L', "0").replace('R', "1");
+//             if let Ok(col) = u16::from_str_radix(&bc, 2) {
+//                 if let Ok(col) = u16::from_str_radix(&bc, 2) {
+//                     println!("{:?} {:?}",c,bc);
+//                     return Ok(Seat { row, col, sid: row * 8u16 + col })
+//                 }
+//             } else {
+//                 return Err("failed to parse col")
+//             }
+//         } else {
+//             return Err("failed to parse row")
+//         }
+//     }).collect()
+// }
+fn parse_input_day1(input: &str) -> Result<Vec<Seat>, ParseIntError> {
+    input
+        .replace('F', "0").replace('B', "1")
+        .replace('L', "0").replace('R', "1")
+        .lines()
+        .map(|cod| {
+            match u16::from_str_radix(cod, 2) {
+                Ok(sid) => Ok(Seat {cod: (*cod).to_string(), sid, row: sid / 8u16, col: sid % 8u16 }),
+                Err(e) => Err(e),
+            }
+        })
+        .collect()
+
 }
 
 #[aoc(day5, part1)]
-fn part1(input: &[u8]) -> usize {
-    reduce(input)
+fn part1(input: &[Seat]) -> u16 {
+    input.iter().fold(0u16, |acc, s| acc.max(s.sid))
 }
 
 #[aoc(day5, part2)]
-fn part2(input: &[u8]) -> Option<usize> {
-    (b'A'..=b'Z')
-        .map(|c| reduce(input.iter().filter(|&&a| a != c && a != c + DIFF)))
-        .min()
-}
-
-#[aoc(day5, part1, Stack)]
-fn part1_stack(input: &[u8]) -> usize {
-    stack(input)
-}
-
-#[aoc(day5, part2, Stack)]
-fn part2_stack(input: &[u8]) -> Option<usize> {
-    (b'A'..=b'Z')
-        .map(|c| stack(input.iter().filter(|&&a| a != c && a != c + DIFF)))
-        .min()
-}
-
-fn reduce<'a>(polymer: impl IntoIterator<Item = &'a u8>) -> usize {
-    let polymer: Vec<_> = polymer
-        .into_iter()
-        .map(|&a| Cell::new(NonZeroU8::new(a)))
-        .collect();
-
-    let mut i = 0;
-    loop {
-        if i + 1 >= polymer.len() {
-            break;
-        }
-
-        let current = &polymer[i];
-        let next = if let Some(b) = polymer[i + 1..].iter().find(|b| b.get().is_some()) {
-            b
+fn part2(input: &[Seat]) -> u16 {
+    let sections = input.iter().fold(Vec::<Range<u16>>::new(), |mut acc, Seat {sid, ..}| {
+        if acc.is_empty() {
+            acc.push(*sid..(*sid+1));
         } else {
-            break;
-        };
-
-        match (current.get(), next.get()) {
-            (Some(c), Some(n)) => {
-                if diff(c.get(), n.get()) == DIFF {
-                    current.set(None);
-                    next.set(None);
-
-                    i = polymer[..i]
-                        .iter()
-                        .enumerate()
-                        .rev()
-                        .find_map(|(i, a)| if a.get().is_some() { Some(i) } else { None })
-                        .unwrap_or_else(|| {
-                            polymer
-                                .iter()
-                                .enumerate()
-                                .find_map(|(i, a)| if a.get().is_some() { Some(i) } else { None })
-                                .unwrap()
-                        })
-                } else {
-                    i = polymer
-                        .iter()
-                        .enumerate()
-                        .skip(i + 1)
-                        .find_map(|(i, a)| if a.get().is_some() { Some(i) } else { None })
-                        .unwrap();
+            let idx = acc.as_slice().binary_search_by(|a| {
+                match *a {
+                    Range { start, end } if (start..end).contains(sid) => Ordering::Equal,
+                    Range { end, .. } if end == *sid => Ordering::Equal,
+                    Range { start, .. } if start == *sid + 1 => Ordering::Equal,
+                    Range { start, .. } if start > *sid => Ordering::Greater,
+                    Range { end, .. } if end < *sid => Ordering::Less,
+                    Range { .. } => unreachable!(),
                 }
+            });
+            match idx {
+                Err(i) => acc.insert(i, *sid..(*sid+1)),
+                Ok(j) => {
+                    let mut idx_range = j..j+1;
+                    let mut arange: Range<u16> = acc[j].start..acc[j].end;
+                    if !acc[j].contains(sid) {
+                        if acc[j].end == *sid {
+                            arange = acc[j].start..*sid + 1;
+                        } else if acc[j].start == *sid + 1 {
+                            arange = *sid..acc[j].end;
+                        }
+                    } 
+                    if let Some(ai) = acc.get(j-1) {
+                        if ai.end == arange.start {
+                            idx_range.start = j-1;
+                            arange = ai.start..arange.end;
+                        }
+                    }
+                    if let Some(ak) = acc.get(j+1) {
+                        if arange.end == ak.start {
+                            idx_range.end = j+2;
+                            arange = arange.start..ak.end;
+                        }
+                    }
+                    acc.splice(idx_range, [arange].iter().cloned());
+                },
             }
-            _ => panic!("{}\n{:#?}", i, polymer),
+
         }
-    }
-
-    polymer.into_iter().filter(|a| a.get().is_some()).count()
-}
-
-fn stack<'a>(polymer: impl IntoIterator<Item = &'a u8>) -> usize {
-    polymer
-        .into_iter()
-        .fold(Vec::new(), |mut stack, &unit| {
-            match stack.last() {
-                Some(&other) if diff(other, unit) == DIFF => {
-                    stack.pop();
-                }
-                _ => stack.push(unit),
-            }
-
-            stack
-        })
-        .len()
+        acc
+    });
+    println!("sections {:?}", sections);
+    sections.get(0).unwrap_or(&(0..1)).end
 }
 
 #[cfg(test)]
